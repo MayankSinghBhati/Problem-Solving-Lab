d8129d93d1fddf3ef3485b758c6769aacacf0e4a
Mayank SIngh Bhati <mayankbhati324@gmail.com>
2026-02-23 21:30:51 +0530

    :)

--- LeetCode/Check If a String Contains All Binary Codes of Size K.py
+++ LeetCode/Check If a String Contains All Binary Codes of Size K.py
@@ -1,10 +1,17 @@
 class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
-        if k>1:
-            for i in range(len(s)):
-                if s[i] == s[i+1]:
-                    return True
-                else:
-                    return False
-        else:
-            return True
+        required_count = 2 ** k
+
+        seen_codes = set()
+
+        for i in range(len(s) - k + 1):
+            substring = s[i : i + k]
+            seen_codes.add(substring)
+            
+            if len(seen_codes) == required_count:
+                return True
+
+        return len(seen_codes) == required_count
+
+# For one line code we can simplify the return statement as follows:
+#         return len({s[i:i+k] for i in range(len(s) - k + 1)}) == 2**k   
